faf193d18bfa0c0f947f4505b61a20081cc9c506
Sahil-Mandal15 <sahilmandal06@gmail.com>
2026-02-21 20:48:10 +0530

    added repository setup

---
+++ composeApp/src/commonMain/kotlin/com/dev/healthreminder/data/repository/UserConfigurationRepositoryImpl.kt
@@ -0,0 +1,44 @@
+package com.dev.healthreminder.data.repository
+
+import com.dev.healthreminder.data.local.UserConfigurationLocalDataSource
+import com.dev.healthreminder.domain.mapper.toModel
+import com.dev.healthreminder.domain.model.ReminderModel
+import com.dev.healthreminder.domain.repository.UserConfigurationRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+
+class UserConfigurationRepositoryImpl(
+    private val dataSource: UserConfigurationLocalDataSource,
+) : UserConfigurationRepository {
+    override fun getAllReminders(): Flow<List<ReminderModel>> =
+        flow {
+            emit(dataSource.getAllReminders().map { it.toModel() })
+        }.flowOn(Dispatchers.Default)
+
+    override suspend fun updateReminderState(
+        reminderId: Long,
+        isActive: Boolean,
+    ) {
+        dataSource.updateReminderState(
+            reminderId,
+            isActive,
+        )
+    }
+
+    override suspend fun updateReminderSetting(
+        reminderId: Long,
+        interval: Long,
+    ) {
+        dataSource.updateReminderSetting(
+            reminderId,
+            interval,
+        )
+    }
+
+    override suspend fun clearReminders() {
+        dataSource.clearReminders()
+    }
+}
