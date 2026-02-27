31b635a3c57e2b6be275bbbbaa4eeb6e9c7fe7a2
Subhadiptech <subhadipdhn@gmail.com>
2026-02-27 23:55:13 +0530

    [Configure Screen] UI Component WIP

---
+++ composeApp/src/commonMain/kotlin/com/dev/healthreminder/presentation/ui/ConfigureScreen.kt
@@ -0,0 +1,12 @@
+package com.dev.healthreminder.presentation.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ConfigureScreen() {
+    Box(modifier = Modifier) {
+
+    }
+}
