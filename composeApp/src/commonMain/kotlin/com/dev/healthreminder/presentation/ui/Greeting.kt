71480e586b1f8d94ffcc067a1117eaab91a7155c
Subhadiptech <subhadipdhn@gmail.com>
2026-03-07 02:52:43 +0530

    [Configure Screen] UI Component WIP

--- composeApp/src/commonMain/kotlin/com/dev/healthreminder/presentation/ui/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.dev.healthreminder.presentation.ui
-
-import com.dev.healthreminder.getPlatform
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String = "Hello, ${platform.name}!"
-}
