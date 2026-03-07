7cc60e409375564ffba3e33ca650528d757bb485
Sahil-Mandal15 <sahilmandal06@gmail.com>
2026-03-08 00:42:34 +0530

    fix: koin init in jvm

--- composeApp/src/jvmMain/kotlin/com/dev/healthreminder/Main.kt
+++ composeApp/src/jvmMain/kotlin/com/dev/healthreminder/Main.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.dev.healthreminder.database.HealthReminderDatabase
+import com.dev.healthreminder.di.initKoin
 import com.dev.healthreminder.presentation.ui.App
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -11,6 +12,8 @@ import org.koin.core.context.GlobalContext
 
 fun main() =
     application {
+        initKoin()
+
         // TODO: Ensure following the best practice for warm building the db
         LaunchedEffect(Unit) {
             withContext(Dispatchers.IO) {
